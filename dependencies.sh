f2f22536ecc29ac0c543841b1928c6acad3567d8
aislingnorris <denoirea@tcd.ie>
2017-03-23 18:14:56 +0000

    dependencies fix

--- dependencies.sh
+++ dependencies.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install tcl
-sudo apt-get install tcl-dev
-sudo apt-get install check
-sudo apt-get install expect
-sudo apt-get install libxml2
-sudo apt-get install libxml2-dev
-sudo apt-get install byacc
-sudo apt-get install flex
-sudo apt-get install libreadlinr-dev
-sudo apt-get install libreadline-gplv2-dev
+sudo apt-get -y install tcl
+sudo apt-get -y install tcl-dev
+sudo apt-get -y install check
+sudo apt-get -y install expect
+sudo apt-get -y install libxml2
+sudo apt-get -y install libxml2-dev
+sudo apt-get -y install byacc
+sudo apt-get -y install flex
+sudo apt-get -y install libreadlinr-dev
+sudo apt-get -y install libreadline-gplv2-dev
 sudo apt-get build-dep build-essential
-sudo apt-get install gcc
-sudo apt-get install g++
+sudo apt-get -y install gcc
+sudo apt-get -y install g++
 
-sudo apt-get install libncurses5-dev
-sudo apt-get install python-pip
-sudo pip install ontospy
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install python-pip
+sudo pip -y install ontospy
 
 cd peos-master
 make
